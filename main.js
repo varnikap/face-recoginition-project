21a74e7ca0ca4b029330c472cddc0d7787f0889e
varnikap <78656240+varnikap@users.noreply.github.com>
2021-09-18 11:44:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,12 +12,34 @@ Webcam.attach(" #camera ");
 function takesnapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML ='<img id="captures_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML ='<img id="selfie_image" src="'+data_uri+'"/>';
 
     });
 }
 
 console.log('m15 version:', ml5.version);
 
-classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/J3et4v2X8/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZxjMu7qYb/model.json',modelLoaded);
+function modelLoaded() 
+{ 
+    console.log('Model Loaded!'); 
+} 
+function identify()
+{
+      img= document.getElementById("selfie_image");
+      classifier.classify(img, gotResult);
+}
+function gotResult(error, results)
 
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("obj").innerHTML = "Object: " + results[0].label;    
+        document.getElementById("acc").innerHTML = "Accurancy: " + results[0].confidence.toFixed(3);
+    }
+
+}
